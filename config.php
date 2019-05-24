cd917a8e5f2695710cc59d11ef937681a8cf1c5b
acerpc <664995777@qq.com>
2019-05-24 16:30:42 +0800

    vv3

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 	小程序支付  配置账号信息
+ *
+ */
+
+
+
+define('APPID', '');
+define('MCHID', "");
+define('KEY', "");
+define('APPSECRET', '');
+
+
+define('SSLCERT_PATH', dirname(__FILE__) . "certs/apiclient_cert.pem");
+define('SSLKEY_PATH', dirname(__FILE__) . "certs/apiclient_key.pem");
+define('NOTIFY_URL', '');
