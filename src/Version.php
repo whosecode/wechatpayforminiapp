fa4b7998a5d6aaffe9c1e1e4bcf3424044607837
acerpc <664995777@qq.com>
2019-05-25 14:39:44 +0800

    vv1

---
+++ src/Version.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace thewings\wechatpayforminiapp;
+
+class Version {
+
+    public function ver(){
+        return 'v1.0.1';
+    }
+
+    public function t01($appid){
+        return 'appid:'.$appid;
+    }
+}
